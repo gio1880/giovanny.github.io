eb0009f06bafc9645828026fc5303c79bf0fbe15
gio1880 <giovannydelosangeles@gmail.com>
2019-08-16 03:25:54 +0000

    update javascript

--- Java Script Sheets/portfolioJavaScript.js
+++ Java Script Sheets/portfolioJavaScript.js
@@ -1,9 +1,11 @@
 function w3_open() {
   document.getElementById("menuBar").style.display = "block";
   $("#openIcon").hide();
+  $(".portfolioImgs").css("position","inherit");
 }
 
 function w3_close() {
   document.getElementById("menuBar").style.display = "none";
   $("#openIcon").show();
+  $(".portfolioImgs").css("position","relative");
 }
