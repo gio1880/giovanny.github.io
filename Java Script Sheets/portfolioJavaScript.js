4a53ae455ee35a4a7f21e6e90c59be45d964a305
gio1880 <giovannydelosangeles@gmail.com>
2019-08-16 03:44:54 +0000

    update portfoio java script

--- Java Script Sheets/portfolioJavaScript.js
+++ Java Script Sheets/portfolioJavaScript.js
@@ -2,6 +2,9 @@ function w3_open() {
   document.getElementById("menuBar").style.display = "block";
   $("#openIcon").hide();
   $(".portfolioImgs").css("position","inherit");
+  $(".portfolioImgs").hover(function(){
+  $(this).css("opacity",0);
+});
 }
 
 function w3_close() {
