f94a797a37bd5e91d9df6d742e95636e0b3d4d7c
gio1880 <giovannydelosangeles@gmail.com>
2019-08-21 00:03:18 +0000

    javascript

--- Java Script Sheets/portfolioJavaScript.js
+++ Java Script Sheets/portfolioJavaScript.js
@@ -1,14 +1,16 @@
 function w3_open() {
-  document.getElementById("menuBar").style.display = "block";
-  $("#openIcon").hide();
-  $(".portfolioImgs").css("position","inherit");
-  $(".portfolioImgs").hover(function(){
-  $(this).css("opacity",0);
-});
+    document.getElementById("menuBar").style.display = "block";
+    $("#openIcon").hide();
+    $(".portfolioImgs").css("position","inherit");
+    $(".portfolioImgs").hover(function(){
+        $(this).css("opacity",0);
+    });
+    $("#portfolioContainer").hide();
 }
 
 function w3_close() {
   document.getElementById("menuBar").style.display = "none";
   $("#openIcon").show();
   $(".portfolioImgs").css("position","relative");
+  $("#portfolioContainer").show();
 }
